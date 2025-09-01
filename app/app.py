962aa6bde91e3a1530b3904812f1dfd14b14bf5b
Andrew <andrewunifei@gmail.com>
2025-09-01 20:47:30 -0300

    feat: logs

--- app/app.py
+++ app/app.py
@@ -1,9 +1,25 @@
+import logging
+from contextlib import asynccontextmanager
 from fastapi import FastAPI
+from sqlalchemy.orm import registry
 
-app = FastAPI()
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+logger.addHandler(logging.StreamHandler())
+
+
+@asynccontextmanager
+async def lifespan(app):
+    logger.info("Iniciando app")
+    yield
+    logger.info("Finalizando app")
+
+
+app = FastAPI(lifespan=lifespan)
+
 
 @app.get("/check")
 def check():
+    logger.info("App ok!")
     return {"status": "ok"}
-
 
