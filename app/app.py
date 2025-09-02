02dd4e2ad681bb848f274171eaee0c7cdb4ce4c5
Andrew <andrewunifei@gmail.com>
2025-09-01 21:13:52 -0300

    feat: database communication

--- app/app.py
+++ app/app.py
@@ -1,17 +1,31 @@
 import logging
 from contextlib import asynccontextmanager
 from fastapi import FastAPI
-from sqlalchemy.orm import registry
+from pydantic import BaseModel
+from sqlalchemy import create_engine
+from sqlalchemy.orm import registry, Mapped, mapped_column, Session
 
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
 logger.addHandler(logging.StreamHandler())
 
+reg = registry()
+engine = create_engine("sqlite:///:memory:")
+
+
+@reg.mapped_as_dataclass
+class Pessoa:
+    __tablename__ = "pessoas"
+    id: Mapped[int] = mapped_column(init=False, primary_key=True)
+    nome: Mapped[str] = mapped_column(default="Luma")
+
 
 @asynccontextmanager
 async def lifespan(app):
     logger.info("Iniciando app")
+    reg.metadata.create_all(bind=engine)
     yield
+    reg.metadata.drop_all(bind=engine)
     logger.info("Finalizando app")
 
 
@@ -23,3 +37,22 @@ def check():
     logger.info("App ok!")
     return {"status": "ok"}
 
+
+class PessoaSchemaIn(BaseModel):
+    nome: str
+
+
+class PessoaSchemaOut(BaseModel):
+    nome: str
+
+
+@app.post("/create", response_model=PessoaSchemaOut)
+def create(pessoa: PessoaSchemaIn):
+    dump = pessoa.model_dump()
+    p = Pessoa(**dump)
+    logger.info("Criando Pessoa", extra=dump)
+    with Session(bind=engine) as session:
+        session.add(p)
+        session.commit()
+        session.refresh(p)
+    return p
